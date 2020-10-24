a9a1c131d0aee1d6314c8702f8a94fd6f6f59f99
Ashley Kwan <ashleykwan888@gmail.com>
2020-10-24 00:32:56 +0000

    finished the lab

--- static/js/tradingCard.jsx
+++ static/js/tradingCard.jsx
@@ -1,11 +1,127 @@
 "use strict";
+// const tradingCardData = [
+//   {
+//     name: 'Balloonicorn',
+//     skill: 'video games',
+//     imgUrl: '/static/img/balloonicorn.jpg'
+//   }
+// ]
+
+// function TradingCard(props) {
+//   return (
+//     <div className="card">
+//       <h2>Name: {props.name}</h2>
+//       <img src={props.imgUrl} />
+//       <h2>Skill: {props.skill}</h2>
+//     </div>
+//   );
+// }
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Balloonicorn"
+//       skill="video games"
+//       imgUrl="/static/img/balloonicorn.jpg"
+//     />
+//   ),
+//   document.querySelector('#balloonicorn')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Float"
+//       skill="baking pretzels"
+//       imgUrl="/static/img/float.jpg"
+//     />
+//   ),
+//   document.querySelector('#float')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Llambda"
+//       skill="knitting scarves"
+//       imgUrl="/static/img/llambda.jpg"
+//     />
+//   ),
+//   document.querySelector('#llambda')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Cat"
+//       skill="swimming"
+//       imgUrl="/static/img/merge.jpg"
+//     />
+//   ),
+//   document.querySelector('#Cat')
+// );
+
+// ReactDOM.render(
+//   (
+//     <TradingCard
+//       name="Melon"
+//       skill="eating"
+//       imgUrl="/static/img/seedypy.jpg"
+//     />
+//   ),
+//   document.querySelector('#Melon')
+// );
+
 const tradingCardData = [
   {
     name: 'Balloonicorn',
     skill: 'video games',
     imgUrl: '/static/img/balloonicorn.jpg'
+  },
+
+  {
+    name: 'Float',
+    skill: 'baking pretzels',
+    imgUrl: '/static/img/float.jpg'
+  },
+
+  {
+    name: 'Llambda',
+    skill: 'knitting scarves',
+    imgUrl: '/static/img/llambda.jpg'
+  },
+
+
+  {
+    name: 'Off-By-One',
+    skill: 'climbing mountains',
+    imgUrl: '/static/img/off-by-one.jpg'
+  },
+
+  {
+    name: 'Seed.py',
+    skill: 'making curry dishes',
+    imgUrl: '/static/img/seedpy.jpg'
+  },
+
+  {
+    name: 'Polymorphism',
+    skill: 'costumes',
+    imgUrl: '/static/img/polymorphism.jpg'
+  },
+
+  {
+    name: 'Short Stack Overflow',
+    skill: 'ocean animal trivia',
+    imgUrl: '/static/img/shortstack-overflow.jpg'
+  },
+
+  {
+    name: 'Merge',
+    skill: 'bullet journaling',
+    imgUrl: '/static/img/merge.jpg'
   }
-]
+];
 
 function TradingCard(props) {
   return (
@@ -17,57 +133,27 @@ function TradingCard(props) {
   );
 }
 
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Balloonicorn"
-      skill="video games"
-      imgUrl="/static/img/balloonicorn.jpg"
-    />
-  ),
-  document.querySelector('#balloonicorn')
-);
+function TradingCardContainer() {
+  const tradingCards = [];
+
+  for (const currentCard of tradingCardData) {
+    tradingCards.push(
+      <TradingCard
+        name={currentCard.name}
+        skill={currentCard.skill}
+        imageUrl={currentCard.imgUrl}
+      />
+    );
+  }
+
+  return (
+    <React.Fragment>
+      {tradingCards}
+    </React.Fragment>
+  );
+}
 
 ReactDOM.render(
-  (
-    <TradingCard
-      name="Float"
-      skill="baking pretzels"
-      imgUrl="/static/img/float.jpg"
-    />
-  ),
-  document.querySelector('#float')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Cat"
-      skill="swimming"
-      imgUrl="/static/img/merge.jpg"
-    />
-  ),
-  document.querySelector('#Cat')
-);
-
-ReactDOM.render(
-  (
-    <TradingCard
-      name="Llambda"
-      skill="knitting scarves"
-      imgUrl="/static/img/llambda.jpg"
-    />
-  ),
-  document.querySelector('#llambda')
+  <TradingCardContainer />,
+  document.querySelector('#container')
 );
